e93a45584adff5134d724391a56aedd9b68ffccf
jiyouliang <18098975621@163.com>
2018-06-16 17:01:26 +0800

    write readme

--- util/mysqlutils.py
+++ util/mysqlutils.py
@@ -3,5 +3,6 @@ import pymysql
 
 def get_connect(db):
     """连接mysql数据库"""
-    connect = pymysql.connect(host="192.168.1.100", user="root", passwd="12345678", db=db, charset="utf8")
+    connect = pymysql.connect(host="172.16.0.128", user="root",
+                              passwd="mysql", db=db, charset="utf8")
     return connect
